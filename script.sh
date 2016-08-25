24ff8cefc4363825c3e03b3395de7117ff07c393
Przemek Suchodolski <psuchodolski@egnyte.com>
2016-08-25 15:04:39 +0200

    Newest Selenium requires java 8

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@
 #=========================================================
 echo "Install the packages..."
 #=========================================================
+sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get -y install fluxbox xorg unzip vim default-jre rungetty firefox
+sudo apt-get -y install fluxbox xorg unzip vim openjdk-8-jre rungetty firefox
 
 #=========================================================
 echo "Set autologin for the Vagrant user..."
